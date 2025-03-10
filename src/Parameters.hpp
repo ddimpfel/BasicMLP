187657d1722dc7b96ea9ba54103265b27d302a73
Dominick Dimpfel <ddimpfel1@gmail.com>
2025-03-09 23:14:07 -0400

    added better dev parameters tracking

---
+++ src/Parameters.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace param
+{
+
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
+int NoiseSize = 128;
+int	NoiseOctaves = 4;
+float fNoiseFreq = 0.01;
+float fNoiseLacunarity = 2.f;
+float fNoiseGain = 0.5f;
+
+float fOffsetMultiplier = 100;
+int VertexMultiplier = 20;
+}
