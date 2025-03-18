91229306681f5b484325b47dcf2a2a8d153ad937
Dominick Dimpfel <ddimpfel1@gmail.com>
2025-03-17 23:13:32 -0400

    created 100 randomized agent vehicles with random initial accelerations

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ static float LossMSE(int n, float pred, float expected)
     return (2.f / static_cast<float>(n)) * (pred - expected);
 }
 
-void BuildNetwork(Network& nn, std::uniform_real_distribution<float>& dist, std::mt19937& gen)
+void BuildNetwork(Network& nn, std::vector<int>& architecture, std::uniform_real_distribution<float>& dist, std::mt19937& gen)
 {
 
     //std::vector<std::vector<std::vector<float>>> weights{
@@ -61,7 +61,7 @@ void BuildNetwork(Network& nn, std::uniform_real_distribution<float>& dist, std:
     //    {{0.6f, 0.8f, 0.1f}, {0.9f, 0.8f, 0.1f}}
     //};
 
-    nn = Network({ 4, 2, 3, 2, 2, 3, 2 }, ActivationSigmoid, DerivativeActivationSigmoid, LossMSE, // weights);
+    nn = Network(architecture, ActivationSigmoid, DerivativeActivationSigmoid, LossMSE, // weights);
         std::make_unique<std::uniform_real_distribution<float>>(dist),
         std::make_unique<std::mt19937>(gen)
     );
@@ -498,7 +498,6 @@ b2BodyId CreateVehicle(b2WorldId world, float halfWidth, float halfHeight)
     return vehicleBody;
 }
 
-
 void VehicleSense(b2WorldId world, b2BodyId vehicleBody, sf::VertexArray& outDrawableRays, std::vector<b2RayResult>& outResults)
 {
     // Body parameters
@@ -528,6 +527,27 @@ void VehicleSense(b2WorldId world, b2BodyId vehicleBody, sf::VertexArray& outDra
         outDrawableRays[i*2] = sf::Vertex{ { origin.x * param::fBOX2D_SCALE, origin.y * param::fBOX2D_SCALE }, sf::Color::Yellow };
         outDrawableRays[i*2+1] = sf::Vertex{ { (origin.x + translation.x) * param::fBOX2D_SCALE, (origin.y + translation.y) * param::fBOX2D_SCALE }, sf::Color::Yellow };
     }
+}
+
+void VehicleMove(b2BodyId vehicleBody, b2Vec2& acceleration)
+{
+    b2Vec2 newVelocity = b2Body_GetLinearVelocity(vehicleBody) + acceleration;
+    b2Body_SetLinearVelocity(vehicleBody, newVelocity);
+    // Box2d handles updating the position in it's world step
+    // new_position = (previous_position + new_velocity) * linear_damping
+}
+
+std::vector<float>& VehicleThink(Network& vehicleBrain, const std::vector<float>& inputs)
+{
+    std::vector<float> outputs = vehicleBrain.Predict(inputs);
+    return outputs;
+}
+
+void VehicleStep(b2BodyId vehicleBody, Network& vehicleBrain, const std::vector<float>& inputs)
+{
+    std::vector<float>& outputs = VehicleThink(vehicleBrain, inputs);
+    b2Vec2 acceleration = { outputs[0], outputs[1] };
+    VehicleMove(vehicleBody, acceleration);
 }
 
 void DrawVehicle(sf::RenderWindow& window, b2BodyId vehicleBody, float halfWidth, float halfHeight)
@@ -585,18 +605,20 @@ int main()
     sf::Clock m_clock;
     sf::Time m_deltaTime;
 
-    //std::random_device rd;
-    //std::mt19937 gen(rd());
-    //std::uniform_real_distribution<float> dist{ 0.f, 1.f };
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist{ -1.f, 1.f };
 
     //std::vector<uint8_t> pixels(param::iWIDTH * param::iHEIGHT * 4);
     //sf::Texture pixelBuffer{ sf::Vector2u(param::iWIDTH, param::iHEIGHT) };
     //sf::Sprite pixelSprite = GeneratePerlinNoiseMap(view, pixelBuffer, pixels);
 
-    //Network nn;
-    //BuildNetwork(nn, dist, gen);
-    //std::vector<float> inputs = { dist(gen), dist(gen), dist(gen), dist(gen) };//, dist(gen), dist(gen), dist(gen) };
-    //std::vector<float> expected = { 0.f, 0.95f };//, 1.f, 0.0f, 0.f, 0.f, 0.95f, 1.f, 0.0f, 0.f, 0.f, 0.95f, 1.f, 0.0f, 0.f, 0.f, 0.95f, 1.f, 0.0f, 0.f, };
+    /*!
+     *  Soft Actor-Critic RL??
+     *  Should outputs be some velocity unit vector U(x, y) and some acceleration pressure?
+     *      Acceleration pressure could be [0,1] * maxAccel, allowing fine tuned control of velocity
+     *      Will this have isses generalizing to different surface conditions?
+     */
 
 
     /*!
@@ -604,24 +626,83 @@ int main()
      */
     size_t vertexCount = TWO_PI * param::iVertexMultiplier;
 
+    // Define physics world (box2d)
     b2WorldDef worldDef = b2DefaultWorldDef();
     worldDef.gravity = { 0.f, 0.f };
     b2WorldId world = b2CreateWorld(&worldDef);
 
+    // Create physics walls from Perlin noise
     Wall wallInner(vertexCount);
     Wall wallOuter(vertexCount);
-
     GenerateSimplexNoisePath(param::iSEED, vertexCount, wallInner.displayVertices, wallOuter.displayVertices, wallInner.physicsVertices, wallOuter.physicsVertices);
-
     wallInner.CreateBody(world);
     wallOuter.CreateBody(world);
 
-    float halfWidth = 0.5f;
-    float halfHeight = 0.25f;
-    b2BodyId vehicleBody = CreateVehicle(world, halfWidth, halfHeight);
-
+    // Init ray sensing and display
     sf::VertexArray rays = sf::VertexArray{ sf::PrimitiveType::Lines, param::iRayCount * 2 };
     std::vector<b2RayResult> rayResults(param::iRayCount);
+
+    // Testing spawning 100 agents and giving them a random velocity
+    float randomSeed = dist(gen); // to avoid all agents starting with same seed
+    float halfWidth = 0.5f;
+    float halfHeight = 0.25f;
+    std::vector<b2BodyId> vehicles(100);
+    for (size_t i = 0; i < 100; i++)
+    {
+        /*!
+         *   Create a randomized agent to interact with the environment
+         */
+        b2BodyId vehicleBody = CreateVehicle(world, halfWidth, halfHeight);
+
+        gen.seed(randomSeed + i);
+        std::vector<int> architecture = { 2, 4, 2 };
+        Network vehicleBrain;
+        BuildNetwork(vehicleBrain, architecture, dist, gen);
+
+        // Determine inputs based on position initially
+        const b2Vec2& vehiclePosition = b2Body_GetPosition(vehicleBody);
+        std::vector<float> inputs = { vehiclePosition.x, vehiclePosition.y };
+
+        // Normalize inputs
+        float xMinArena = INFINITY, xMaxArena = -INFINITY;
+        float yMinArena = INFINITY, yMaxArena = -INFINITY;
+        for (auto& vertex : wallOuter.physicsVertices)
+        {
+            if (xMinArena > vertex.x)
+                xMinArena = vertex.x;
+            if (xMaxArena < vertex.x)
+                xMaxArena = vertex.x;
+            if (yMinArena > vertex.y)
+                yMinArena = vertex.y;
+            if (yMaxArena < vertex.y)
+                yMaxArena = vertex.y;
+        }
+        inputs[0] = (inputs[0] - xMinArena) / (xMaxArena - xMinArena);
+        inputs[1] = (inputs[1] - yMinArena) / (yMaxArena - yMinArena);
+
+        // Agent moves the vehicle
+        // Network outputs are normalized from [0, 1]
+        std::vector<float> outputs = vehicleBrain.Predict(inputs);
+
+        // Transform outputs to represent all possible acceleration vectors
+        //  ouputs in range from [-0.5 to 0.5)
+        if (outputs[0] < 0.5f)
+            outputs[0] *= -1.f;
+        else // outputs[0] >= 0.5f
+            outputs[0] -= 0.5f;
+
+        if (outputs[1] < 0.5f)
+            outputs[1] *= -1.f;
+        else // outputs[1] >= 0.5f
+            outputs[1] -= 0.5f;
+
+        // Move agent
+        b2Vec2 acceleration = { outputs[0], outputs[1] };
+        b2Vec2 newVelocity = b2Body_GetLinearVelocity(vehicleBody) + acceleration;
+        b2Body_SetLinearVelocity(vehicleBody, newVelocity);
+
+        vehicles[i] = vehicleBody;
+    }
 
     int framecounter = 0;
     while (m_window.isOpen())
@@ -634,7 +715,7 @@ int main()
         m_deltaTime = m_clock.restart();
         ImGui::SFML::Update(m_window.get(), m_deltaTime);
         b2World_Step(world, m_deltaTime.asSeconds(), 4);
-        VehicleSense(world, vehicleBody, rays, rayResults);
+        //VehicleSense(world, vehicleBody, rays, rayResults);
 
         // Debug windows
         ImGui::ShowMetricsWindow();
@@ -642,7 +723,7 @@ int main()
         //ShowPerlinNoiseWindow(pixelSprite, view, pixelBuffer, pixels);
         //ShowPerlinNoiseLoopWindow(vertices, vertexCount);
         ShowSimplexNoisePathWindow(param::iSEED, vertexCount, wallInner, wallOuter);
-        ShowVehicleStatsWindow(vehicleBody);
+        //ShowVehicleStatsWindow(vehicleBody);
         
         m_window.ProcessEvents(view); // Also processes ImGui events
 
@@ -651,7 +732,12 @@ int main()
         m_window.Draw(wallInner.displayVertices);
         m_window.Draw(wallOuter.displayVertices);
         m_window.Draw(rays);
-        DrawVehicle(m_window.get(), vehicleBody, halfWidth, halfHeight);
+        // Testing running 100 agents
+        for (size_t i = 0; i < 100; i++)
+        {
+            DrawVehicle(m_window.get(), vehicles[i], halfWidth, halfHeight);
+        }
+        //DrawVehicle(m_window.get(), vehicleBody, halfWidth, halfHeight);
 
         //m_window.Draw(pixelSprite);
         //DrawNetwork(nn, m_window, view.getCenter() / 2.f, 20.f);
